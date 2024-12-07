3ad5068086d6da24cfc04e491227414ac7887d59
FreyNell <freybalaguera@gmail.com>
2024-12-07 18:19:20 -0500

    Starting Point

---
+++ start.ps1
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+param (
+    [Parameter(Mandatory = $true)]
+    [string]$Environment # Argumento para especificar el ambiente (dev, prod, etc.)
+)
+
+$envFile = ".\secrets\$($Environment).env"
+
+if (-Not (Test-Path $envFile)) {
+    Write-Error "El archivo $envFile no existe."
+    exit 1
+}
+
+Get-Content $envFile | ForEach-Object {
+
+    # Omitir líneas en blanco o comentarios
+    if ($_ -match '^\s*#' -or $_ -match '^\s*$') {
+        return
+    }
+
+    # Dividir la línea en clave=valor
+    $parts = $_ -split '=', 2
+
+    if ($parts.Length -eq 2) {
+        $key = $parts[0].Trim()
+        $value = $parts[1].Trim()
+
+        # Establecer la variable de entorno
+        [System.Environment]::SetEnvironmentVariable($key, $value, "Process")
+    }
+
+}
+
+cd .\devops\iac
+
+docker-compose up
